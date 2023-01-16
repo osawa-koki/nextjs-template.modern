81ff0bf84672345ddadef2b7d923d370e1af5865
osawa-koki <github@osawa.dev>
2023-01-16 23:25:01 +0900

    絵文字プロパティ。

--- pages.ts
+++ pages.ts
@@ -2,14 +2,17 @@ import Setting from "./setting";
 
 const pages = [
   {
+    emoji: "🏠",
     path: "/",
     name: "Home",
   },
   {
+    emoji: "📖",
     path: "/about/",
     name: "About",
   },
   {
+    emoji: "💓",
     path: "/contact/",
     name: "Contact",
   },
